cf5592cc0f9b6a16940f5ce2cdbf6407fdc69486
newbiestatguy <azimulh@gmail.com>
2020-05-20 22:20:45 +0300

    added required files for the python library Baseline

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='Baseline',
+    name='BaselineCorrection',
     version='0.0.1',
     description='Implementation of Modified polyfit method and IModPoly method for baseline correction',
     author='StatguyUser',
-    url='https://github.com/StatguyUser/Baseline',
+    url='https://github.com/StatguyUser/BaselineCorrection',
     install_requires=['numpy','sklearn'],
-    download_url='https://github.com/StatguyUser/Baseline.git',
-    py_modules=["Baseline"],
+    download_url='https://github.com/StatguyUser/BaselineCorrection.git',
+    py_modules=["BaselineCorrection"],
     package_dir={'':'src'},
 )
